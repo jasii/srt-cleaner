a16d92bbb042a7424f11c308b59754f62e639276
Joseph <sepej@oregonstate.edu>
2025-02-05 11:14:34 -0800

    set up for production

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def download():
     return send_file("output.txt", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
